187415b2be71337d0c0139b8e8f2d5afa2da7b76
lucaspalleros <148975450+lucaspalleros@users.noreply.github.com>
2024-04-01 12:11:41 -0300

    Feat/integration sent messages (#15)
    * feat: message-inbox api response integration + loader
    
    * add: sent list table with new keys values + loader

--- src/app/messages/sent/page.tsx
+++ src/app/messages/sent/page.tsx
@@ -1,15 +1,42 @@
+"use client"
+
+import React, { useEffect } from "react";
 import { Box, Paper } from "@mui/material";
 import DataTable from "../../component/inbox-table";
 import InboxHeader from "@/app/component/inbox-header";
-import { rows } from "./mock"
 import { columnsSent } from "@/app/component/inbox-table/constants";
+import { SentData } from "@/app/component/inbox-table/type";
+import { mockResponse } from "./response-mock";
 
 export default function SentScreen() {
+  const [isLoading, setIsLoading] = React.useState<boolean>(true);
+  const [data, setData] = React.useState<SentData[]>(mockResponse);
+
+  const fetchData = async () => {
+    try {
+      setIsLoading(true);
+      const response = await fetch('/api/message?type='); // TODO: endpoint api/message is not ready inside this project. Remove this when the endpoint is ready.
+      if (!response.ok) {
+        setIsLoading(false);
+        throw new Error("Error al solicitar mensajes");
+      }
+      setData(mockResponse); // TODO: MOCK API Sinacofi-V5 - remove this when the endpoint is configured inside NEXT.
+      setIsLoading(false);
+    } catch (error) {
+      console.error("Error al solicitar mensajes", error);
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  },[]);
+
   return (
     <Paper sx={{ width: "calc(100% - 270px)" }}>
       <Box sx={{ m: 2 }}>
-        <InboxHeader amountMessages={rows.length} title={'Mensajes Enviados'}/>
-        <DataTable rows={rows} columns={columnsSent}/>
+        <InboxHeader amountMessages={data.length} title={'Mensajes Enviados'}/>
+        <DataTable rows={data} columns={columnsSent} loading={isLoading} />
       </Box>
     </Paper>
   );
