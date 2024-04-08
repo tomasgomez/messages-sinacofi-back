6d964276560d48da2c7fd2439748d9534185c45f
Tomás Gómez Molina <tomas@conomyhq.com>
2024-04-08 16:04:31 -0400

    add(updateMessage): create a copy of the message with a new status

--- src/backend/usecases/message/updateMessage.ts
+++ src/backend/usecases/message/updateMessage.ts
@@ -1,10 +1,32 @@
 import { Message } from "@/backend/entities/message";
 import { MessageRepository } from "@/backend/interfaces/messageRepository";
+import { fetchServerResponse } from "next/dist/client/components/router-reducer/fetch-server-response";
 
 
 // Create message function
 export async function updateMessage(repository: MessageRepository, message: Message): Promise<Message | Error> {
     try {
+        if (message.status && message.status === "05") {
+            let fetchedMessage = await repository.find(message, true, "1", "1");
+
+            if (fetchedMessage instanceof Error) {
+                return fetchedMessage;
+            }
+
+            if (fetchedMessage.length === 0) {
+                return new Error("No message found");
+            }
+
+            let messageCopy = fetchedMessage[0];
+
+            // Update message status
+            messageCopy.status = "06";
+
+            // Create message with different status and id
+            let createdMessage = await repository.create(messageCopy);
+
+            console.log("Created message: ", createdMessage);
+        }
         let messageResponse = await repository.update(message);
         return messageResponse;
     } catch (error: any) {
--- src/utils/messagesStatus.js
+++ src/utils/messagesStatus.js
@@ -1,8 +1,5 @@
 const messageStatus = [
    { "code": "01", "description": "PREPARADO" },
-    { "code": "02", "description": "ENVIADO" },
-    { "code": "03", "description": "ASIGNADO" },
-    { "code": "04", "description": "APROBADO" },
-    { "code": "05", "description": "VERIFICADO" },
-    { "code": "06", "description": "RECHAZADO" }
+    { "code": "05", "description": "ENVIADO" },
+    { "code": "06", "description": "BANDEJA DE ENTRADA" }
 ];
