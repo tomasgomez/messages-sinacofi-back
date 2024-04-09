f59e250af2017ba8cbd0854a8e63e67c65fa29de
Tomás Gómez Molina <tomas@conomyhq.com>
2024-04-09 13:09:24 -0400

    add(docker)

--- src/backend/repository/find.ts
+++ src/backend/repository/find.ts
@@ -34,10 +34,6 @@ export async function find(message: Message, detail: boolean, count: string, off
     if (messageCode) where.messageCode = messageCode;
     if (status) where.status = status;
 
-    if (status === '06') {
-      where.sender = "CORP BANCA" // TODO: add filter with institution in front"
-    }
-
     console.log('where', where);
 
     let select = {
