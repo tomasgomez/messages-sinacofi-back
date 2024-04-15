784cd6e855f8203c09c0ec13706f9a32348ecb1e
MatiasLenio <matias.pereyra@leniolabs.com>
2024-04-15 14:38:25 -0300

    feat(card): implement card wiouth expand data

--- src/app/mortgage-discharge/in-process/page.tsx
+++ src/app/mortgage-discharge/in-process/page.tsx
@@ -1,17 +1,21 @@
-"use client"
-
+"use client";
 import React from "react";
-
 import { Box, Paper } from "@mui/material";
-
 import Header from "./header";
+import CarDischarge from "@/app/mortgage-discharge/components/card";
+import { dataList } from "./mock";
 
 export default function InProcessScreen() {
   return (
     <Paper sx={{ width: "calc(100% - 270px)" }}>
-      <Box sx={{ m: 2 }}> 
-        <Header title={'Alzamientos Hipotecarios en Proceso'} />
+      <Box sx={{ m: 2 }}>
+        <Header title={"Alzamientos Hipotecarios en Proceso"} />
+      </Box>
+      <Box>
+        {dataList.map((data) => (
+          <CarDischarge data={data} />
+        ))}
       </Box>
     </Paper>
   );
-};
+}
