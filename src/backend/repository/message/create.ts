f3318a38e688ed4412a10ab675cefec592452581
Tomás Gómez Molina <tomas@conomyhq.com>
2024-04-23 13:45:45 -0400

    add(connection): connected to the rules and admin on the cloud

--- src/backend/repository/message/create.ts
+++ src/backend/repository/message/create.ts
@@ -8,40 +8,39 @@ import {
 
 async function create(message: Message): Promise < Message | Error > {
     try {
-        // const prisma = new PrismaClientWrapper();
+        const prisma = new PrismaClientWrapper();
 
-        // const prismaClient = prisma.getClient();
+        const prismaClient = prisma.getClient();
 
-        // /* Empty message object */
-        // let messageData: Message = new Message();
+        /* Empty message object */
+        let messageData: Message = new Message();
         
-        // if (message.setTime) {
-        //     message.setTime();
-        // }
+        if (message.setTime) {
+            message.setTime();
+        }
 
-        // if (Object.keys(message).length > 0) {
-        //     for (const [key, value] of Object.entries(message)) {
-        //         if (value !== undefined && key in message) {
-        //             messageData[key as keyof Message] = value;
-        //         }
-        //     }
-        // } else {
-        //     console.error('No message data provided');
-        //     return new Error('No message data provided');
-        // }
+        if (Object.keys(message).length > 0) {
+            for (const [key, value] of Object.entries(message)) {
+                if (value !== undefined && key in message) {
+                    messageData[key as keyof Message] = value;
+                }
+            }
+        } else {
+            console.error('No message data provided');
+            return new Error('No message data provided');
+        }
 
-        // /* Create a new message */
-        // const newMessage = await prismaClient.message.create({
-        //     data: messageData
-        // });
+        /* Create a new message */
+        const newMessage = await prismaClient.message.create({
+            data: messageData
+        });
 
-        // if (newMessage === null) {
-        //     console.error('Error creating message');
-        //     return new Error('Error creating message');
-        // }
+        if (newMessage === null) {
+            console.error('Error creating message');
+            return new Error('Error creating message');
+        }
 
-        // return newMessage as Message;
-        return new Message();
+        return newMessage as Message;
     } catch (error: any) {
         console.error('Error creating message:', error);
         return error;
