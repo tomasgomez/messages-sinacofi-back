63ff79f6400f7e748af3f917398d43604b710a38
Lucas Palleros <lucas.palleros@leniolabs.com>
2024-05-08 10:01:45 -0300

    add: Parameter[] interface

--- src/backend/handler/message/presenter/createMessage.ts
+++ src/backend/handler/message/presenter/createMessage.ts
@@ -1,3 +1,4 @@
+import { Parameter } from '@/backend/entities/message/interface';
 import { Message } from '@/backend/entities/message/message';
 
 export function validateCreateMessage(data: any): Message | Error {
@@ -8,6 +9,8 @@ export function validateCreateMessage(data: any): Message | Error {
   }
 
   let message: Message = new Message();
+  let parametersMessage: Parameter[]  = [];
+
   message.messageCode = messageCode.trim();
 
   if (sender && typeof sender === 'string' && sender.trim() !== '') {
@@ -22,13 +25,38 @@ export function validateCreateMessage(data: any): Message | Error {
     message.receiver = receiver.trim();
   }
 
-  if (parameters && typeof parameters === 'object') {
-    message.parameters = parameters;
+
+  if (cukCode && typeof cukCode === 'string' && cukCode.trim() !== '') {
+    message.cukCode = cukCode.trim();
   }
 
   if (cukCode && typeof cukCode === 'string' && cukCode.trim() !== '') {
     message.cukCode = cukCode.trim();
   }
+
+  if (parameters && typeof parameters === 'object') {
+     parameters.forEach((element: any) => {
+      let parameter: Parameter =  {
+        id: element.id ?? '',
+        name: element.name ?? '',
+        messageCode: element.messageCode ?? '',
+        label: element.label ?? '',
+        type: element.type ?? '',
+        placeholder: element.placeholder ?? '',
+        description: element.description ?? '',
+        defaultValue: element.defaultValue ?? '',
+        priority: element.defaultValue ?? 0,
+        value: element.value ?? '',
+        properties: element.properties ?? '',
+        validations: element.validations ?? '',
+        createdAt: element.createdAt ?? Date(),
+        updatedAt: element.updatedAt ?? Date(),
+        messageId: element.messageId ?? '',
+      }
+      parametersMessage.push(parameter)
+     });
+  }
+  
 
   return message;
 }
