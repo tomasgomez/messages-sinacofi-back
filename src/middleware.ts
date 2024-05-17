e6d6111ac837f01155be72dd8ab7dc0478e4587c
Lucas Palleros <lucas.palleros@leniolabs.com>
2024-05-17 12:42:03 -0300

    add: url oci

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,10 +5,8 @@ import { NextRequest } from 'next/server'
 // This function can be marked `async` if using `await` inside
 export async function middleware(request: NextRequest, event: NextFetchEvent) {
     console.log('# middleware')
-    // if(false) {
-    //   return NextResponse.redirect(new URL('http://localhost:3000/api/callback', request.url))
-    // }
-    event.waitUntil(
+      return NextResponse.redirect(new URL('https://idcs-1e0f415dd2bf423c8296ebb063528eca.identity.oraclecloud.com/ui/v1/signin', request.url))
+      event.waitUntil(
       fetch(`http://localhost:3000/api/auth`, {
         method: 'GET',
       })
