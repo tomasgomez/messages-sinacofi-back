b5b85f0ee2cd537abba50fc5847714bf2519b001
Lucas Palleros <lucas.palleros@leniolabs.com>
2024-05-17 12:42:03 -0300

    add: url oci

--- src/backend/handler/auth/handler.ts
+++ src/backend/handler/auth/handler.ts
@@ -6,10 +6,10 @@ import { get } from "./get";
 /*
 Auth hanlder
 */
-class auhHandler extends Handler implements APICalls {
+class auhtHandler extends Handler implements APICalls {
     // Auth
     GET = async (req: NextApiRequest, res: NextApiResponse < any > ) => get(req, res);
 }
 
 // export Auth calls
-export const auhHandlerCalls: APICalls = new auhHandler();
+export const auhHandlerCalls: APICalls = new auhtHandler();
