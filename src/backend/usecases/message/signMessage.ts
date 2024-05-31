69e4b1bbdefd15bec350480f62e8f6c533dc4717
Tomás Gómez Molina <tomas@conomyhq.com>
2024-05-31 17:46:50 -0400

    fix(sig)

--- src/backend/usecases/message/signMessage.ts
+++ src/backend/usecases/message/signMessage.ts
@@ -1,25 +1,37 @@
-import { isForeclosureMessageCode } from "@/backend/entities/cuk/codes";
-import { CUK } from "@/backend/entities/cuk/cuk";
+import {
+    CUK
+} from "@/backend/entities/cuk/cuk";
 import {
     Message
 } from "@/backend/entities/message/message";
 import {
     MessageRepository
 } from "@/backend/repository/messageRepository";
-import { MessageStatus } from "@/utils/messagesStatus";
-import { updateForclosure } from "../foreclosure/updateForeclosure";
-import { CUKRepository } from "@/backend/repository/cukRepository";
+import {
+    MessageStatus
+} from "@/utils/messagesStatus";
+import {
+    updateForclosure
+} from "../foreclosure/updateForeclosure";
+import {
+    CUKRepository
+} from "@/backend/repository/cukRepository";
+import {
+    MessageTypes
+} from "@/backend/entities/message/types";
+import {
+    ForeclosureStatus
+} from "@/backend/entities/cuk/codes";
 
 
 // Create message function
 export async function signMessage(repository: MessageRepository, cukRepository: CUKRepository, message: Message): Promise < Message | Error > {
     try {
-        let status = '';  
+        let status = '';
         let updatedCuk: CUK | Error;
 
-
         if (message.statusCode && message.statusCode !== undefined && message.id !== undefined && message.setStatus) {
-            
+
             status = message.statusCode;
 
             message.setStatus(status);
@@ -51,15 +63,13 @@ export async function signMessage(repository: MessageRepository, cukRepository: 
             return messageResponse;
         }
 
-        if (messageResponse.messageCode && isForeclosureMessageCode(messageResponse.messageCode)) {
+        if (messageResponse.messageCode === MessageTypes.ALZAMIENTO_HIPOTECARIO && status === MessageStatus.ENVIADO) {
             let cuk = new CUK();
             cuk.cukCode = messageResponse.cukCode;
-
-            if (message.getStatus) // TODO: aca chequear segun el estado del mensaje y el codigo del mensaje en que estado deberia quedar el cuk
-            cuk.status = message.getStatus() ?? '';
-
+            cuk.status = ForeclosureStatus.INIT
+            
             updatedCuk = await updateForclosure(cukRepository, repository, cuk, message);
-
+            
             if (updatedCuk instanceof Error) {
                 console.error('Error updating CUK:', updatedCuk);
                 return updatedCuk;
