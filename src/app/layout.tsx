fbbf1964d67036efac94e5af89a8dd40d022accf
Erick Gomez <erick.gomez@leniolabs.com>
2024-05-31 16:03:03 -0400

    feat: remove modalState from the appcontext

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,17 +14,6 @@ import { MessageExportProvider } from "./component/MessageExportProvider";
 import { SessionProvider, SessionProviderContext } from "@/context/SessionProvider";
 
 
-const clearObjet = {
-  type: "none" as "none",
-  title: "",
-  body: <></>,
-  isOpen: false,
-  onClose: () => null,
-  onConfirm: () => null,
-  onRetry: () => null,
-  props: {},
-};
-
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -33,16 +22,6 @@ export default function RootLayout({
   // Change after add users "selectedInstitution"
   const [selectedInstitution, setSelectedInsitution] = useState();
   const [currentInstitution, setCurrentInstitution] = useState();
-  const [modalState, setModalState] = useState<{
-    type: "success" | "error" | "decision" | "none";
-    title: string;
-    body?: React.ReactNode;
-    isOpen: boolean;
-    onClose?: () => void;
-    onConfirm?: () => void;
-    onRetry?: () => void;
-    props?: any;
-  }>(clearObjet);
 
   useEffect(() => {
     // Perform localStorage action
@@ -59,7 +38,6 @@ export default function RootLayout({
               <ThemeProvider theme={theme}>
                 <MyContexLayout.Provider
                   value={{
-                    setModalState,
                     selectedInstitution,
                     setSelectedInsitution,
                     currentInstitution,
