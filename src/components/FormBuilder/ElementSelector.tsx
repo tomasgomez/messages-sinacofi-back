02fed5753cbe45b32f23c893cf21f44fe8927b0a
MatiasLenio <matias.pereyra@leniolabs.com>
2024-06-03 11:53:14 -0300

    fix(build): fix build

--- src/components/FormBuilder/ElementSelector.tsx
+++ src/components/FormBuilder/ElementSelector.tsx
@@ -10,36 +10,52 @@ import PasswordField from "./fields/PasswordField";
 import PhoneNumberField from "./fields/PhoneNumberField";
 import Accordion from "./labels/Accordion";
 
-
 const Select = (props: any) => {
   const { setValue } = useFormContext();
-  const onChange = (value) => {
+  const onChange = (value: unknown) => {
     setValue(props.id, value);
-  }
-  return (
-    <Dropdown {...props} onChange={onChange}/>
-  )
-}
+  };
+  return <Dropdown {...props} onChange={onChange} />;
+};
 const FieldTypes = {
   textField: Field,
-  checkbox: (props: any) => <Checkbox {...props} options={props.properties.options} />,
+  checkbox: (props: any) => (
+    <Checkbox {...props} options={props.properties.options} />
+  ),
   date: DatePickerInput,
   select: Select,
   textArea: (props: any) => <Field {...props} multiline />,
   dni: RutField,
   phoneNumber: PhoneNumberField,
   amount: AmountField,
-  password: PasswordField
+  password: PasswordField,
 };
 
 const LabelTypes = {
   linebreak: (props: any) => (
     <Box sx={{ position: "relative", overflow: "hidden" }}>
-      <Typography variant="caption" sx={{ backgroundColor: "#EFFCFF", color: "#565656", padding: "4px" }}>
+      <Typography
+        variant="caption"
+        sx={{ backgroundColor: "#EFFCFF", color: "#565656", padding: "4px" }}
+      >
         {props.label}
       </Typography>
-      <svg width="100%" height="1" viewBox="0 0 100% 1" fill="none" xmlns="http://www.w3.org/2000/svg" style={{ position: "absolute", top: "50%" }}>
-        <line x1="4.37114e-08" y1="0.5" x2="100%" y2="0.500094" stroke="#898989" stroke-dasharray="5 5"/>
+      <svg
+        width="100%"
+        height="1"
+        viewBox="0 0 100% 1"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+        style={{ position: "absolute", top: "50%" }}
+      >
+        <line
+          x1="4.37114e-08"
+          y1="0.5"
+          x2="100%"
+          y2="0.500094"
+          stroke="#898989"
+          stroke-dasharray="5 5"
+        />
       </svg>
     </Box>
   ),
@@ -47,32 +63,32 @@ const LabelTypes = {
     <Box>
       {props.label.split("\\n").map((label: any) => (
         <Typography variant={props?.properties?.variant || "body2"} key={label}>
-          {label}<br/>
+          {label}
+          <br />
         </Typography>
       ))}
     </Box>
-  )
+  ),
 };
 
-
-
-const ElementSelector = ({ type, props }: { type: any, props: any }) => {
-  const FieldGotten = FieldTypes[type as keyof typeof FieldTypes] || FieldTypes.textField;
+const ElementSelector = ({ type, props }: { type: any; props: any }) => {
+  const FieldGotten =
+    FieldTypes[type as keyof typeof FieldTypes] || FieldTypes.textField;
 
   if (type === "accordion") {
-    return (
-      <Accordion {...props} />
-    )
-  };
+    return <Accordion {...props} />;
+  }
 
   if (type === "linebreak" || type === "label") {
-    const Label = LabelTypes[type as keyof typeof LabelTypes] || LabelTypes.label;
-    return (
-      <Label {...props} />
-    );
-  };
+    const Label =
+      LabelTypes[type as keyof typeof LabelTypes] || LabelTypes.label;
+    return <Label {...props} />;
+  }
 
-  const { validations: { disabled, ...rules }, ...inputProps } = props; 
+  const {
+    validations: { disabled, ...rules },
+    ...inputProps
+  } = props;
 
   return (
     <Controller
@@ -80,19 +96,19 @@ const ElementSelector = ({ type, props }: { type: any, props: any }) => {
       control={inputProps.control}
       defaultValue={inputProps.defaultValue}
       rules={rules}
-      render={({ field, }) => {
+      render={({ field }) => {
         return (
           <FieldGotten
             error={!!props.errors?.[inputProps.id]}
             {...inputProps}
             {...field}
             // {...validations}
-            label={rules?.required ? `* ${inputProps.label}` : inputProps.label }
+            label={rules?.required ? `* ${inputProps.label}` : inputProps.label}
           />
-        )
+        );
       }}
     />
   );
-}
+};
 
 export default ElementSelector;
--- src/context/SessionProvider.tsx
+++ src/context/SessionProvider.tsx
@@ -1,27 +1,24 @@
-'use client';
+"use client";
 
-import React, { useEffect, createContext, useState, useCallback  } from "react";
-
-
+import React, { useEffect, createContext, useState, useCallback } from "react";
 
 export interface User {
-  role:            string;
-  name:            string;
+  role: string;
+  name: string;
   institutionCode: string;
-  area:            string;
-  email:           string;
-  status:          string;
+  area: string;
+  email: string;
+  status: string;
 }
 
 export interface UserInfo {
-  user:        User;
+  user: User;
   permissions: { [key: string]: boolean };
 }
 type initialUserInfoType = {
-  userInfo: UserInfo | undefined,
-  setUserInfo: React.Dispatch<React.SetStateAction<UserInfo | undefined>>,
+  userInfo: UserInfo | undefined;
+  setUserInfo: React.Dispatch<React.SetStateAction<UserInfo | undefined>>;
 };
-
 
 const initialUserInfoState: initialUserInfoType = {
   userInfo: undefined,
@@ -30,35 +27,38 @@ const initialUserInfoState: initialUserInfoType = {
 
 export const SessionProviderContext = createContext(initialUserInfoState);
 
-export const SessionProvider = ({ children }: { children: any}) => {
+export const SessionProvider = ({ children }: { children: any }) => {
+  const [userInfo, setUserInfo] = useState<initialUserInfoType | undefined>(
+    undefined
+  );
 
-    const [userInfo, setUserInfo] = useState<initialUserInfoType | undefined>(undefined);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("/api/permissions");
+        const fetchedData = await response.json();
+        setUserInfo(fetchedData);
+      } catch (err) {
+        console.log(err);
+      }
+    };
 
-    useEffect(() => {
-        const fetchData = async () => {
-          try {
-            const response = await fetch('/api/permissions');
-            const fetchedData = await response.json();
-            setUserInfo(fetchedData);
-          } catch (err) {
-            console.log(err);
-          }
-        };
-    
-        fetchData();
-      }, []);
+    fetchData();
+  }, []);
 
-    const contextValue = React.useMemo(
-        () => ({
-            userInfo, setUserInfo
-        }),
-        [
-            userInfo, setUserInfo
-        ],
-    );
-    return (
-        <SessionProviderContext.Provider value={contextValue}>
-            {children}
-        </SessionProviderContext.Provider>
-    );
+  const contextValue = React.useMemo(
+    (): initialUserInfoType => ({
+      userInfo: userInfo as UserInfo | undefined,
+      setUserInfo: setUserInfo as React.Dispatch<React.SetStateAction<UserInfo | undefined>>,
+    }),
+    [userInfo, setUserInfo]
+  );
+  
+  return (
+    <SessionProviderContext.Provider
+      value={contextValue}
+    >
+      {children}
+    </SessionProviderContext.Provider>
+  );
 };
