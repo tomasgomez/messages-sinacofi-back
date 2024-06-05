5434012a607de70c25747977310cb26463c9448c
MatiasLenio <matias.pereyra@leniolabs.com>
2024-06-05 12:05:16 -0300

    fix(dimension): fix dimension

--- src/utils/dimensions.tsx
+++ src/utils/dimensions.tsx
@@ -1,25 +1,27 @@
 import { useEffect, useMemo, useState } from "react";
 
 export const useCalcDimensions = (usedHeight?: number, usedWidth?: number) => {
-  const [windowHeight, setWindowHeight] = useState<number>(0);
-  const [windowWidth, setWindowWidth] = useState<number>(0);
-
+  const [windowHeight, setWindowHeight] = useState<number>(
+    typeof window !== "undefined" ? window.innerHeight : 0
+  );
+  const [windowWidth, setWindowWidth] = useState<number>(
+    typeof window !== "undefined" ? window.innerWidth : 0
+  );
+  
   // useEffect to update the state when the window size changes
   useEffect(() => {
-    if (typeof window !== "undefined") {
-      const handleResize = () => {
-        setWindowHeight(window?.innerHeight);
-        setWindowWidth(window?.innerWidth);
-      };
+    const handleResize = () => {
+      setWindowHeight(window?.innerHeight);
+      setWindowWidth(window?.innerWidth);
+    };
 
-      // Add the event listener
-      window.addEventListener("resize", handleResize);
+    // Add the event listener
+    window.addEventListener("resize", handleResize);
 
-      // Clean up the event listener when the component unmounts
-      return () => {
-        window.removeEventListener("resize", handleResize);
-      };
-    }
+    // Clean up the event listener when the component unmounts
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
   }, []);
 
   // useMemo to calculate the value of maxHeight and maxWidth only when windowHeight or windowWidth changes
