676734714e91c84fe7ca070fcb049c377d3b5913
MatiasLenio <matias.pereyra@leniolabs.com>
2024-06-10 10:39:18 -0300

    feat(loading): add loader after login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,19 @@
-'use client'
-import { useSession, signIn } from 'next-auth/react';
-import {  useRouter } from 'next/navigation';
-import { useEffect } from 'react';
-
+"use client";
+import Loader from "@/components/Loader";
+import { useSession, signIn } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Home() {
   const router = useRouter();
   const { data: session, status } = useSession();
-  const loading = status === 'loading';
+  const loading = status === "loading";
 
   useEffect(() => {
     if (!loading && !session) {
-      signIn('oidc', { prompt: "login" })
-    }
-    else if (session && !loading){
-      router.push('/messages/inbox');
+      signIn("oidc", { prompt: "login" });
+    } else if (session && !loading) {
+      router.push("/messages/inbox");
     }
   }, [session, loading]);
 
@@ -22,7 +21,5 @@ export default function Home() {
     return null; // If no session and still loading, do not render anything
   }
 
-  return (
-    <div>loading...</div>
-  );
-};
+  return <Loader label="iniciando sesión…" minHeight={"100vh"} />;
+}
